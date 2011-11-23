f1f0aa12d63b71adc1e6eea4369907cfbfe65b0f
Roee Kremer <roikr75@gmail.com>
2011-11-23 09:14:55 +0200

    facebook new view controller - to merge

---
+++ iOSUtils/RateMeMessage/RateMeMessage.m
@@ -0,0 +1,13 @@
+//
+//  RateMeMessage.m
+//  Gogos
+//
+//  Created by Roee Kremer on 11/22/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "RateMeMessage.h"
+
+@implementation RateMeMessage
+
+@end
