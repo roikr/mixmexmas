5ac0f1937376b930dda739651d53acd69a0340f0
Roee Kremer <roikr75@gmail.com>
2012-02-09 20:50:31 +0200

    fixes after integration milgrom

---
+++ Classes/ApplicationKeys.h
@@ -0,0 +1,11 @@
+//
+//  ApplicationKeys.h
+//  MilgromLight
+//
+//  Created by Roee Kremer on 2/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#pragma once
+
+extern NSString* const kApplicationFont;
